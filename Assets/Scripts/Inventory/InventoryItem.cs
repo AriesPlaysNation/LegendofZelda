2aec16373b44dd9ee7b5c4938448cb5820ba91b8
AriesNation <bradbotteron13@gmail.com>
2019-05-30 22:08:06 -0400

    Inventory Addition!!
    Adding Inventory(not quite done yet)

---
+++ Assets/Scripts/Inventory/InventoryItem.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Inventory/Items")]
+public class InventoryItem : ScriptableObject
+{
+    public string itemName;
+    public string itemDesc;
+    public Sprite itemImage;
+    public int numberHeld;
+    public bool usable;
+    public bool unique;
+}
